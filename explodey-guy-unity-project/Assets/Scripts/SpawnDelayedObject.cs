6804cc55b5a488d612b504c9b5d7ffd224044906
Hollow-Cube4444 <159060858+ACardamone4@users.noreply.github.com>
2024-11-15 23:15:50 -0600

    camera shake acquired

--- explodey-guy-unity-project/Assets/Scripts/SpawnDelayedObject.cs
+++ explodey-guy-unity-project/Assets/Scripts/SpawnDelayedObject.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class SpawnDelayedObject : MonoBehaviour
 {
-    [SerializeField] private float _waitTime;
+    [SerializeField] private float _waitToDie;
+    [SerializeField] private float _waitToSpawn;
     [SerializeField] private GameObject _spawnedObject;
 
     public void Awake()
@@ -13,8 +14,9 @@ public class SpawnDelayedObject : MonoBehaviour
 
     private IEnumerator Spawning()
     {
-        yield return new WaitForSeconds(_waitTime);
+        yield return new WaitForSeconds(_waitToSpawn);
         GameObject TNTOutline = Instantiate(_spawnedObject, gameObject.transform.position, gameObject.transform.rotation);
+        yield return new WaitForSeconds(_waitToDie);
         Destroy(gameObject);
     }
 }
