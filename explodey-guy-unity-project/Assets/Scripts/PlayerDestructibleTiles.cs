cffb8cc75304d403b0791275dd8f42dd86ac065c
Hollow-Cube4444 <159060858+ACardamone4@users.noreply.github.com>
2024-12-03 19:11:33 -0600

    wtf

--- explodey-guy-unity-project/Assets/Scripts/PlayerDestructibleTiles.cs
+++ explodey-guy-unity-project/Assets/Scripts/PlayerDestructibleTiles.cs
@@ -13,7 +13,25 @@ public class PlayerDestructibleTiles : MonoBehaviour
         destructibleTileMap = GetComponent<Tilemap>();
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        Vector3 hitPosition = Vector3.zero;
+    //        foreach (ContactPoint2D hit in collision.contacts)
+    //        {
+    //            hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+    //            hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+    //            destructibleTileMap.SetTile(destructibleTileMap.WorldToCell(hitPosition), null);
+    //            print("HitPosition =" + hitPosition);
+
+    //            TestSpawn.position = new Vector3(hitPosition.x, hitPosition.y);
+    //            GameObject BlockRefresh = Instantiate(TestObject, TestSpawn.position, TestSpawn.rotation);
+    //        }
+    //    }
+    //}
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
