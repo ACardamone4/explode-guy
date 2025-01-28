1d9f534ebee6dbffd5a981044dfdd7debedfdb6b
Hollow-Cube4444 <159060858+ACardamone4@users.noreply.github.com>
2025-01-28 14:22:51 -0600

    no idea

--- explodey-guy-unity-project/Assets/Scripts/TramStates.cs
+++ explodey-guy-unity-project/Assets/Scripts/TramStates.cs
@@ -3,4 +3,19 @@ using UnityEngine;
 public class TramStates : MonoBehaviour
 {
     [SerializeField] private Animator _traminator;
+
+    public void Idle()
+    {
+        _traminator.Play
+    }
+
+    public void Shake()
+    {
+
+    }
+
+    public void Move()
+    {
+        
+    }
 }
