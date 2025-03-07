edeafee8d336d0d83438c44734a37c9ef02748dc
Hollow-Cube4444 <159060858+ACardamone4@users.noreply.github.com>
2025-03-07 00:55:22 -0600

    checkpoint audio

--- explodey-guy-unity-project/Assets/Scripts/FUSE/CheckAnimController.cs
+++ explodey-guy-unity-project/Assets/Scripts/FUSE/CheckAnimController.cs
@@ -1,16 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckAnimController : MonoBehaviour
 {
     [SerializeField] private Animator _checkpointAnimator;
+    public AudioManager audioManager;
+    public GameObject audioManagerObject;
+    private bool activated;
+
+    private void Start()
+    {
+        audioManagerObject = GameObject.Find("Audio Manager");
+        if (audioManagerObject != null)
+        {
+            audioManager = audioManagerObject.GetComponent<AudioManager>();
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && !activated)
         {
+            activated = true;
             _checkpointAnimator.SetBool("Activate", true);
+            audioManager.CheckpointAudio();
         }
     }
 }
